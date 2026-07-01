a0a84d5116ada6c6e3fb0fed3624186958f4dfc5
Tu-999 <145636736+Tu-999@users.noreply.github.com>
2026-07-01 10:41:48 +0700

    Add files via upload

---
+++ build/CMakeFiles/include/version.h
@@ -0,0 +1,16 @@
+#define PLUGIN_NAME statusbar
+#define PLUGIN_CATALOG_NAME wxS("opencpn-statusbar_pi")
+#define PLUGIN_VERSION_MAJOR 1
+#define PLUGIN_VERSION_MINOR 1
+#define PLUGIN_VERSION_PATCH 12
+#define PLUGIN_VERSION_TWEAK 0
+#define PLUGIN_VERSION_DATE "23/01/2024"
+#define PLUGIN_COMMON_NAME "StatusBar"
+#define PLUGIN_SHORT_DESCRIPTION "StatusBar Plugin is an optional replacement for the builtin statusbar"
+#define PLUGIN_LONG_DESCRIPTION "The builtin status bar (disable from the User Interface tab) has limited configuration options and can be very difficult to read. The statusbar plugin improves on some of these difficulties. Best used with OpenGL enabled (requires some basic OpenGL extensions)."
+#define PLUGIN_PACKAGE_NAME "statusbar_pi"
+// API version of OCPN to use
+#define OCPN_API_VERSION_MAJOR 1
+#define OCPN_API_VERSION_MINOR 17
+// Include extra version settings if required
+
---
+++ build/pkg_version.sh
@@ -0,0 +1,11 @@
+#
+# File is 'sourced' in script files to ensure the correct environment variables are created
+#
+
+VERSION=1.1.12.0.
+VERSION_DATE="2024-01-23 22:22:06 -0400"
+PKG_TARGET=msvc
+PKG_TARGET_VERSION=10.0.26200
+
+# Opencpn api version, part of win32 installer name (major * 100 + minor).
+OCPN_API_VERSION=1.17
